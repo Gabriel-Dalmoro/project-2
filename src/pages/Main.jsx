4d464105ff45811c867312da8c353bad4ad78a0f
Gabriel H. Dalmoro <ghdalmoro@gmail.com>
2022-11-16 13:17:36 -0700

    test new branch

---
+++ src/pages/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import Body from '../components/Body.jsx'
+// import Head from '../components/Head.jsx'
+// import Title from '../components/Title.jsx'
+import { Switch, Route } from 'react-router-dom';
+
+
+const Main = () => (
+    <Switch>
+      <Route path='/' component={Home}></Route>
+      <Route path='/about' component={About}></Route>
+      <Route path='/contact' component={Contact}></Route>
+    </Switch>
+  );
+
+export default Main
+
+//blah
