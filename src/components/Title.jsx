587e943e8f80335115896f4edcdfba3e5d8255de
Gabriel H. Dalmoro <ghdalmoro@gmail.com>
2022-11-16 13:15:49 -0700

    CHORE: add random things

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Title() {
   return (
     <>
       {/* <script src="https://kit.fontawesome.com/83f09b8128.js" crossOrigin="anonymous"></script> */}
       <header>
+        <h1 id='main-title'><NavLink to='/'>FunActive</NavLink></h1>
         <h3>Search</h3>
-        <h1 id='main-title'>FunActive</h1>
-        <h3>About</h3>
-        <h3>Login</h3>
+        <h3><NavLink to='/about'>About</NavLink></h3>
+        <h3><NavLink to='/login'>Login</NavLink></h3>
       </header>
       <p id='subtitle'>
         What would you like to do today?
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import Body from '../components/Body.jsx'
-import Head from '../components/Head.jsx'
-import Title from '../components/Title.jsx'
+// import Body from '../components/Body.jsx'
+// import Head from '../components/Head.jsx'
+// import Title from '../components/Title.jsx'
+import { Switch, Route } from 'react-router-dom';
 
-const Main = () => {
-  return (
-    <div>
-        <Head />
-        <Title />
-        <Body />
-    </div>
-  )
-}
+
+const Main = () => (
+    <Switch>
+      <Route path='/' component={Home}></Route>
+      <Route path='/about' component={About}></Route>
+      <Route path='/contact' component={Contact}></Route>
+    </Switch>
+  );
 
 export default Main
