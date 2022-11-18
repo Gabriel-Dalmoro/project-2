98e06ccfa2509fa3c8914dddcfc2394730ebd95a
George <gwmkwan@gmail.com>
2022-11-18 11:40:26 -0700

    feat: add backend; add mock data.

---
+++ project-2-backend/server.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { mockData } from './mockWeather.js';
+
+dotenv.config();
+
+const PORT = 5001;
+const app = express();
+
+// app.use(cors());
+
+// Create endpoint
+// app.get('/slow', (req, res) => {
+//   console.log('delay for 1 seconds');
+//   setTimeout(() => {
+//     let seconds = new Date().getTime();
+//     res.send({ currentTime: seconds });
+//   });
+// });
+
+// const APIKEY = process.env.API_KEY_GAB;
+
+// const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=calgary&units=metric&appid=${APIKEY}`;
+
+const getWeather = async () => {
+  // const response = await fetch(weatherUrl);
+  // const data = await response.json();
+
+  console.log(mockData.name);
+  console.log(mockData.weather[0].description);
+  return {
+    location: mockData.name,
+    conditions: mockData.weather[0].description,
+    temperature: mockData.main.temp.toFixed(),
+    feels_like: mockData.main.feels_like.toFixed(),
+    wind_speed: mockData.wind.speed.toFixed(),
+    humidity: mockData.main.humidity,
+    // location: data.name,
+    // conditions: data.weather[0].description,
+    // temperature: data.main.temp.toFixed(),
+    // feels_like: data.main.feels_like.toFixed(),
+    // wind_speed: data.wind.speed.toFixed(),
+    // humidity: data.main.humidity,
+  };
+};
+app.get('/weather', async (req, res) => {
+  try {
+    const currentWeather = await getWeather();
+    res.send(currentWeather);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send();
+  }
+});
+
+app.listen(5001, () => console.log(`Listening on ${PORT}`));
