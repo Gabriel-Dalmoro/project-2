44c18c83ebe97ac118a84fe4921001394c87a6fa
George <gwmkwan@gmail.com>
2022-11-19 11:11:38 -0700

    build: created activities file

---
+++ project-2-frontend/src/activities.js
@@ -0,0 +1,49 @@
+const activities = [
+  {
+    id: 1,
+    name: 'Baseball/Softball',
+    imgURL: '',
+    minTemp: 15,
+    maxTemp: 35,
+    maxWind: 15,
+    Precipitation: 2,
+  },
+  {
+    id: 2,
+    name: 'Basketball',
+    imgURL: '',
+    minTemp: 18,
+    maxTemp: 35,
+    maxWind: 10,
+    Precipitation: 2,
+  },
+  {
+    id: 3,
+    name: 'Beach volleyball',
+    imgURL: '',
+    minTemp: 18,
+    maxTemp: 38,
+    maxWind: 12,
+    Precipitation: 2,
+  },
+  {
+    id: 4,
+    name: 'Cycling',
+    imgURL: '',
+    minTemp: 15,
+    maxTemp: 38,
+    maxWind: 15,
+    Precipitation: 2,
+  },
+  {
+    id: 5,
+    name: 'Bouldering',
+    imgURL: '',
+    minTemp: 15,
+    maxTemp: 35,
+    maxWind: 20,
+    Precipitation: 2,
+  },
+];
+
+export default activities;
