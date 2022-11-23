979d4882eb37a114ae6d8ad0934be6517508179e
George <gwmkwan@gmail.com>
2022-11-23 16:12:54 -0700

    refactor: merge main to george repo

--- project-2-frontend/src/components/Body.jsx
+++ project-2-frontend/src/components/Body.jsx
@@ -2,47 +2,25 @@ import React from 'react';
 import activities from '../activities.js';
 import MUIcards from './MUIcards.jsx';
 
-<<<<<<< HEAD
 function Body({ weatherData }) {
-  // if (weatherData.temperature > activityItem.minTemp) {
+  const filteredActivities = activities.filter(el => el.minTemp <= 20);
 
   return (
     <div className="mainBody">
-      {activities.map(activityItem => (
-        <MUIcards
-          key={activityItem.id}
-          name={activityItem.name}
-          season={activityItem.season}
-          maxTemp={activityItem.maxTemp}
-          minTemp={activityItem.minTemp}
-          wind={activityItem.maxWind}
-        />
-      ))}
+      <div className="filteredCards">
+        {filteredActivities.map(activityItem => (
+          <MUIcards
+            key={activityItem.id}
+            name={activityItem.name}
+            season={activityItem.season}
+            maxTemp={activityItem.maxTemp}
+            minTemp={activityItem.minTemp}
+            wind={activityItem.maxWind}
+            url={activityItem.url}
+          />
+        ))}
+      </div>
     </div>
   );
 }
-
-=======
-function Body({weatherData}) {
-  const filteredActivities = activities.filter(el => el.minTemp <= 20);
-  
-      return (
-        <div className="mainBody">
-          <div className="filteredCards">
-            {filteredActivities.map(activityItem => (
-              <MUIcards
-                key={activityItem.id}
-                name={activityItem.name}
-                season={activityItem.season}
-                maxTemp={activityItem.maxTemp}
-                minTemp={activityItem.minTemp}
-                wind={activityItem.maxWind}
-                url={activityItem.url}
-              />
-            ))}
-          </div>
-        </div>
-      );
-    }
->>>>>>> fee6b1862ed0e2312e81eba8d052e8240e45d927
 export default Body;
