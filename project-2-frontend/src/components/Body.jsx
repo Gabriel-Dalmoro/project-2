0d1ad1e5ee8bc36dc9572e48c882c69f4d4365d5
Gabriel H. Dalmoro <ghdalmoro@gmail.com>
2022-11-23 14:46:24 -0700

    feat: add fitlering feature to display activity cards based on current temperature

--- project-2-frontend/src/components/Body.jsx
+++ project-2-frontend/src/components/Body.jsx
@@ -3,23 +3,25 @@ import activities from '../activities.js';
 import MUIcards from './MUIcards.jsx';
 
 function Body({weatherData}) {
-    // if (weatherData.temperature > activityItem.minTemp) {
-     
-
-  return (
-    <div className="mainBody">
-      {activities.map(activityItem => (
-        <MUIcards
-          key={activityItem.id}
-          name={activityItem.name}
-          season={activityItem.season}
-          maxTemp={activityItem.maxTemp}
-          minTemp={activityItem.minTemp}
-          wind={activityItem.maxWind}
-        />
-      ))}
-    </div>
-  );
+  const filteredActivities = activities.filter(el => el.minTemp < weatherData.temperature);
+  
+  for (let i = 0; i < activities.length; i++){
+    if (weatherData.temperature > activities[i].minTemp) {
+      return (
+        <div className="mainBody">
+          {filteredActivities.map(activityItem => (
+            <MUIcards
+              key={activityItem.id}
+              name={activityItem.name}
+              season={activityItem.season}
+              maxTemp={activityItem.maxTemp}
+              minTemp={activityItem.minTemp}
+              wind={activityItem.maxWind}
+            />
+          ))}
+        </div>
+      );
+    }
+  }
 }
-
 export default Body;
