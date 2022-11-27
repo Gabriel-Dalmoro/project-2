ecde929eb83cc52451bdc5538e10d9c2102870fe
George <gwmkwan@gmail.com>
2022-11-26 23:37:16 -0700

    feat: setup files that make request to backend

---
+++ project-2-frontend/src/features/auth/authSlice.js
@@ -0,0 +1,104 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import authService from './authService';
+
+// Get user from localstorage
+const user = JSON.parse(localStorage.getItem('user'));
+
+const initialState = {
+  // Only use if there is a user
+  user: user ? user : null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+};
+// Register new user
+export const register = createAsyncThunk(
+  'auth/register',
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      // If something went wrong, an error message would return
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+// Login user
+export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
+  try {
+    return await authService.login(user);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    // If something went wrong, an error message would return
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
+// Logout user
+export const logout = createAsyncThunk('auth/logout', async () => {
+  await authService.logout();
+});
+
+// createSlice accepts an initial state, an object of reducer functions, and a "slice name", and automatically generates action creators and action types that correspond to the reducers and state.
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    // If it is successful, reset it back to default state
+    reset: state => {
+      state.isLoading = false;
+      state.isError = false;
+      state.isSuccess = false;
+      state.message = '';
+    },
+  },
+  extraReducers: builder => {
+    // When it is pending
+    builder
+      .addCase(register.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+      })
+      .addCase(login.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+      })
+      .addCase(logout.fulfilled, state => {
+        state.user = null;
+      });
+  },
+});
+
+// Export is required whenever an action has been created
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
