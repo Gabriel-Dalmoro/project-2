35b9aeb73831229a9d2fa4550945d0afc711c26e
George <gwmkwan@gmail.com>
2022-11-26 23:07:09 -0700

    docs: backend login registration complete

--- project-2-backend/server.js
+++ project-2-backend/server.js
@@ -10,7 +10,7 @@ import connectDB from './config/db.js';
 
 dotenv.config();
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 // Connect to database
 connectDB();
@@ -46,6 +46,11 @@ const getWeather = async () => {
     // humidity: data.main.humidity,
   };
 };
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to Funactive' });
+});
+
 app.get('/weather', async (req, res) => {
   try {
     const currentWeather = await getWeather();
