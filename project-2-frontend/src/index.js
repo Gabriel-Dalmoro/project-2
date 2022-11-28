fb8c8096f7375801fda9eac95b80cd41e5cacb19
George <gwmkwan@gmail.com>
2022-11-28 10:31:08 -0700

    docs: add comments into LoginRegister

--- project-2-frontend/src/index.js
+++ project-2-frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 import App from './App.js';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store.js';
