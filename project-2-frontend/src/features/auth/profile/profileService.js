4b7f68fb452ffacdbe33f199b15724ed5f13f556
George <gwmkwan@gmail.com>
2022-11-30 09:02:42 -0700

    feat: add profile page and components

---
+++ project-2-frontend/src/features/auth/profile/profileService.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_URL = '/api/profile/';
+
+// Get user Profile
+const getProfile = async token => {
+  const config = {
+    //Token needs to be in the headers
+    headers: { Authorization: `Bearer ${token}` },
+  };
+  const response = await axios.get(API_URL, config);
+
+  return response.data;
+};
+
+const profileService = { getProfile };
+
+export default profileService;
