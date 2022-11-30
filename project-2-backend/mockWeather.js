eda3ddf25948341eeb898786bf97a54a4d80c1cf
George <gwmkwan@gmail.com>
2022-11-30 15:02:04 -0700

    test: change the temp for testing

--- project-2-backend/mockWeather.js
+++ project-2-backend/mockWeather.js
@@ -1,44 +1,44 @@
 export const mockData = {
-  "coord": {
-    "lon": -114.0853,
-    "lat": 51.0501
+  coord: {
+    lon: -114.0853,
+    lat: 51.0501,
   },
-  "weather": [
+  weather: [
     {
-      "id": 800,
-      "main": "Clear",
-      "description": "clear sky",
-      "icon": "01d"
-    }
+      id: 800,
+      main: 'Clear',
+      description: 'clear sky',
+      icon: '01d',
+    },
   ],
-  "base": "stations",
-  "main": {
-    "temp": -2.73,
-    "feels_like": -8.28,
-    "temp_min": -5.63,
-    "temp_max": -1.1,
-    "pressure": 1024,
-    "humidity": 36
+  base: 'stations',
+  main: {
+    temp: -20.73,
+    feels_like: -8.28,
+    temp_min: -5.63,
+    temp_max: -1.1,
+    pressure: 1024,
+    humidity: 36,
   },
-  "visibility": 10000,
-  "wind": {
-    "speed": 4.92,
-    "deg": 286,
-    "gust": 7.6
+  visibility: 10000,
+  wind: {
+    speed: 4.92,
+    deg: 286,
+    gust: 7.6,
   },
-  "clouds": {
-    "all": 7
+  clouds: {
+    all: 7,
   },
-  "dt": 1668794984,
-  "sys": {
-    "type": 2,
-    "id": 2011327,
-    "country": "CA",
-    "sunrise": 1668783487,
-    "sunset": 1668815116
+  dt: 1668794984,
+  sys: {
+    type: 2,
+    id: 2011327,
+    country: 'CA',
+    sunrise: 1668783487,
+    sunset: 1668815116,
   },
-  "timezone": -25200,
-  "id": 5913490,
-  "name": "Calgary",
-  "cod": 200
-}
+  timezone: -25200,
+  id: 5913490,
+  name: 'Calgary',
+  cod: 200,
+};
