1c24961cdd2362927075f05bef6a66f15a0c17dc
Gabriel H. Dalmoro <ghdalmoro@gmail.com>
2022-12-01 12:28:49 -0700

    feat: show user favorite activities filtered

--- project-2-frontend/src/components/Body.jsx
+++ project-2-frontend/src/components/Body.jsx
@@ -1,38 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import activities from '../activities.js';
 import MUIcards from './MUIcards.jsx';
 
 function Body({ weatherData, user }) {
-  // console.log(weatherData);
-  // console.log(activities);
 
-  const [filteredActivities, setFilterActivities] = useState(
-    activities.filter(el => el.minTemp <= weatherData.temp)
-  );
-  console.log(filteredActivities);
-  useEffect(() => {
-    if (user) {
-      console.log(user);
-      setFilterActivities(
-        activities.filter(
-          a => a.minTemp <= weatherData.temp || user.bookmarks.includes(a.id)
-        )
-      );
-    } else {
-      setFilterActivities(
-        activities.filter(el => el.minTemp <= weatherData.temperature)
-      );
-    }
-  }, [user, weatherData]);
+  const userActivities = activities.filter(el => user?.bookmarks?.includes(el.id))
+  const weatherFilteredActivities = activities.filter(el => el.minTemp <= weatherData.temperature)
 
-  console.log(weatherData);
   return (
     <div className="mainBody">
+      { user && <h1 className="weatherRecTitle">These are your favorite activities:</h1> }
+      { user && <div className="filteredCards">
+          {userActivities.map(activityItem => (
+            <MUIcards
+              key={activityItem.id}
+              name={activityItem.name}
+              season={activityItem.season}
+              maxTemp={activityItem.maxTemp}
+              minTemp={activityItem.minTemp}
+              wind={activityItem.maxWind}
+              icon={activityItem.icon}
+              url={activityItem.url}
+            />
+          ))}
+      </div>}
       <h1 className="weatherRecTitle">
         Based on the weather, these activities are recommended:
       </h1>
       <div className="filteredCards">
-        {filteredActivities.map(activityItem => (
+        {weatherFilteredActivities.map(activityItem => (
           <MUIcards
             key={activityItem.id}
             name={activityItem.name}
