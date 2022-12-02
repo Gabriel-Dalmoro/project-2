11f2b16ed7e5c6443b968a0aeacd60a0ab63d48a
George <gwmkwan@gmail.com>
2022-12-02 08:03:08 -0700

    feat: add info in about page

--- project-2-frontend/src/pages/About.jsx
+++ project-2-frontend/src/pages/About.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div syle={{height:'1200px'}}>
-      <h1 className='aboutTitle'>About us</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque necessitatibus, rerum dolor ex magnam cumque dolore cupiditate explicabo quasi, deleniti accusantium aliquid blanditiis quis vitae! Doloremque molestias illo tempore illum veniam hic iste laborum nisi enim maxime iure beatae ex quidem, eos laudantium cumque ut assumenda, voluptatem saepe! Voluptate autem numquam tempora amet excepturi saepe, dolor ipsum natus doloribus ut iste praesentium magni laborum soluta est fugit velit voluptatum corporis quasi neque vel iusto molestias illum. Molestias dicta corrupti at.</p>
+    <div syle={{ height: '1200px' }}>
+      <h1 className="aboutTitle">About us</h1>
+      <p>
+        This app was developed to assist users in determining which outdoor
+        activities, such as basketball, volleyball, golf, skiing, etc., are
+        appropriate given the current weather conditions.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
