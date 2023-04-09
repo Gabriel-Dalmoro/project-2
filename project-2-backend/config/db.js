36a1acb2714685e135fef0c1c686985175aaaaec
George <gwmkwan@gmail.com>
2023-04-09 09:32:36 -0600

    feat: improve catch error when connect to database

--- project-2-backend/config/db.js
+++ project-2-backend/config/db.js
@@ -1,13 +1,19 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    mongoose.set('strictQuery', false);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.log(`Error: ${error.message}`.red.underline.bold);
-    // Exit when connection failed
-    process.exit(1);
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    throw error;
   }
 };
 
